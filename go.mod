34c8c3e1622c9660e777d154b97b91276b61aca2
Siam Rahman <siamr.bro99@gmail.com>
2023-02-02 18:34:35 -0500

    Initialize project modules and packages

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/siamr902/calculus-cli
+
+go 1.19
+
+require github.com/TheDemx27/calculus v0.0.0-20170802071712-c9ac6b2b2cc0 // indirect
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"flag"
+	"math/rand"
+	"strconv"
+	"time"
+	clc "github.com/TheDemx27/calculus"
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	
+}
