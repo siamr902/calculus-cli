e7b9693e610f8e512df6cf52b70385b09ae19027
Siam Rahman <siamr.bro99@gmail.com>
2023-02-02 21:19:11 -0500

    Added average value over the interval

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"flag"
+	"fmt"
+	clc "github.com/TheDemx27/calculus"
+	"log"
 	"math/rand"
 	"regexp"
 	"time"
-	clc "github.com/TheDemx27/calculus"
 )
 
 func main() {
@@ -17,6 +17,7 @@ func main() {
 	diffPoint := flag.Float64("x", 1, "Differentiate the equation at a value.")
 	upperBound := flag.Float64("u", 20, "Set the upper bound for the definite integral.")
 	lowerBound := flag.Float64("l", 0, "Set the lower bound for the definite integral.")
+	averageValue := flag.Bool("avg", false, "Find the average value over the integral")
 	flag.Parse()
 
 	r, err := regexp.Compile("^[A-Za-z0-9*/^+-/(/)]+$")
@@ -26,7 +27,7 @@ func main() {
 
 	// for accurate results, enclose variables within parentheses (e.g. 3x -> 3(x) or 9x^3 -> 9(x^3))
 	matched := r.Match([]byte(*equation))
-	
+
 	if matched == false {
 		log.Fatal("Equation contains invalid characters.")
 	}
@@ -36,10 +37,9 @@ func main() {
 		ans := evaluateDerivative(equation, diffPoint)
 		fmt.Printf("The derivative at the point %v is %v", *diffPoint, ans)
 	case "integral":
-		ans := evaluateIntegral(equation, upperBound, lowerBound)
-		fmt.Printf("The integral from %v to %v is %v", *lowerBound, *upperBound, ans)
+		ans := evaluateIntegral(equation, upperBound, lowerBound, averageValue)
+		fmt.Printf("The integral from %v to %v of the function is %v", *lowerBound, *upperBound, ans)
 	}
-	
 }
 
 func evaluateDerivative(equation *string, point *float64) float64 {
@@ -48,8 +48,12 @@ func evaluateDerivative(equation *string, point *float64) float64 {
 	return ans
 }
 
-func evaluateIntegral(equation *string, upper *float64, lower *float64) float64 {
+func evaluateIntegral(equation *string, upper *float64, lower *float64, averageValue *bool) float64 {
 	f := clc.NewFunc(*equation)
 	ans := f.AntiDiff(*lower, *upper)
+
+	if *averageValue {
+		return ans / (*upper - *lower)
+	}
 	return ans
 }
